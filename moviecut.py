65f03a7f590d4108e43857e4ecfc210dc9d31ffb
Yoshihito T <101462925+Siguretuki@users.noreply.github.com>
2023-12-04 16:27:39 +0900

    Add files via upload

---
+++ moviecut.py
@@ -0,0 +1,24 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+def cut_video(input_file, output_file, duration):
+    # 動画ファイルを読み込む
+    video_clip = VideoFileClip(input_file)
+
+    # 指定した秒数で動画を切り取る
+    remaining_clip = video_clip.subclip(duration)
+
+    # 残りの動画を保存する
+    remaining_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
+
+if __name__ == "__main__":
+    # 入力ファイルのパス
+    input_file_path = "./sample_video_data/test360p.mp4"
+
+    # 出力ファイルのパス
+    output_file_path = "./sample_video_data/test360pCut.mp4"
+
+    # 切り取りたい秒数をテキスト入力で受け取る
+    duration = float(input("残したい秒数を入力してください: "))
+
+    # 動画を切り取る
+    cut_video(input_file_path, output_file_path, duration)
