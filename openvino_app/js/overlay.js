7bf75cff8a6cc71a44c923c9c092c96153845fb6
Siguretuki <siguretukikohane@gmail.com>
2024-01-15 11:55:21 +0900

    オーバーレイ機能追加

---
+++ openvino_app/js/overlay.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var overlayButton = document.getElementById("overlayButton");
+  var overlay = document.getElementById("overlay");
+  var closeOverlayButton = document.getElementById("closeOverlayButton");
+
+  overlayButton.addEventListener("click", function () {
+    overlay.style.display = "flex";
+    setTimeout(function () {
+      var result = confirm(
+        "推論が完了しました。結果ページに移動します。ここでキャンセルすると、推論結果は破棄され最初から開始されます。"
+      );
+      if (result) {
+        window.location.href = "result.html";
+      }
+    }, 5000);
+  });
+
+  closeOverlayButton.addEventListener("click", function () {
+    overlay.style.display = "none";
+  });
+});
